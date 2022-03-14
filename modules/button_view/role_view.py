3cf4afd6dd7d480bfc440eabc02cdd14a5f0af59
Soni <the.original.soni@gmail.com>
2022-03-14 11:48:40 +0530

    Add eyl's code

--- modules/button_view/role_view.py
+++ modules/button_view/role_view.py
@@ -33,3 +33,56 @@ class RoleViewButton(nextcord.ui.Button):
                 f"Your channel is ready at {a.mention}", ephemeral=True
                 )
 
+
+bot.persistent_views_added = False
+
+GUILD_ID = 563104255242797060
+
+ROLE_IDS = [
+    563105950588993538,
+    588650216773779477,
+    883103471954448385,
+]
+
+class RoleButton(nextcord.ui.Button):
+    def __init__(self, role: nextcord.Role):
+        super().__init__(
+            label=role.name,
+            style=nextcord.ButtonStyle.blurple,
+            custom_id=f"RoleView:{role.id}",
+        )
+        self.role = role
+
+    async def callback(self, interaction: nextcord.Interaction):
+        if self.role not in interaction.user.roles:
+            await interaction.send(f"You have been given the {self.role.name} role")
+            await interaction.user.add_roles(self.role)
+        else:
+            await interaction.send(f"Your {self.role.name} role has been removed")
+            await interaction.user.remove_roles(self.role)
+
+
+class RoleView(nextcord.ui.View):
+    def __init__(self, guild: nextcord.Guild, role_ids: List[int]):
+        super().__init__(timeout=None)
+        for role_id in role_ids:
+            role = guild.get_role(role_id)
+            if not role:
+                print(f"Role not found: {role_id}")
+                continue
+            self.add_item(RoleButton(role))
+
+
+@bot.listen()
+async def on_ready():
+    if not bot.persistent_views_added:
+        guild = bot.get_guild(GUILD_ID)
+        bot.add_view(RoleView(guild, ROLE_IDS))
+        bot.persistent_views_added = True
+        print("Persistent views added")
+
+
+@bot.command()
+async def roleview(ctx: commands.Context):
+    await ctx.send(view=RoleView(ctx.guild, ROLE_IDS))
+
